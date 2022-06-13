9c1e38cca374e3553ef8ea0bd2d80357b823c2f4
Peter Kienle <94712916+peterkienle@users.noreply.github.com>
2022-06-13 14:21:53 -0400

    Update mt_export.bash

--- OpenScad Code/V11/mt_export.bash
+++ OpenScad Code/V11/mt_export.bash
@@ -1,33 +1,26 @@
 #!/bin/bash
 
+# V2
 # Peter Kienle, CNS
-#    with help from Mike Gallant
-# V1 2022-6-2
-# - loop to iterate through millitome creation property lists
-# - NOTE: blocksizeIDs are not used to configure openscad; but as look up IDs 
-# 2022-6-7
-# - log output to files; code cleanup; commenting
-#
+# 
+#2022-6-11
+# - export files to correctly named folders
+# - zip/compress folder (and delete folder afterwards)
+
 # run in terminal:
 # - navigate terminal to folder (V11) using 'cd'
 # - bash mt_export.bash
 # - creates output folders if not already present; will replace the content
 
-consoleOutput="_logfile.txt"        # console gets saved to this temp file to be extracted for .csv files
-stlOutputFolder="exports_stl/"      # STLs export folder; automatically created if not present
-csvOutputFolder="exports_csv/"      # CSVs export folder; automatically created if not present
+consoleOutput="_logfile.txt"        # console gets logged to this temp file to be extracted for .csv files
+outputFolder="exports"              # main output folder
 mtGenerator="MT-Generator.scad"     # openscad program code
 outputFlag=2                        # openscad console output: 0=everything, 1=full inserts, 2=column/row inserts only (use this)
 
-# check if export folders exist; if not, create them
-if [ ! -d ${stlOutputFolder} ]
+# check if main export folder exists; if not, create it
+if [ ! -d ${outputFolder} ]
 then
-    mkdir ${stlOutputFolder}
-fi
-
-if [ ! -d ${csvOutputFolder} ]
-then
-    mkdir ${csvOutputFolder}
+    mkdir ${outputFolder}
 fi
 
 # IDs used to configure Openscad output. Must match 'exposed properties' in MT_Generator
@@ -49,7 +42,7 @@ scaleList=(Large Medium Small)
 asciiList=(A B C D E F G H I J K L M N O P Q R S T U V W X Y Z) # lookup table for column IDs
 
 # loop to iterate through complete ID lists
-# - runs openscad with correct config properties for each organ; saves STL files with proper filename to stlOutputFolder
+# - runs openscad with correct config properties for each organ; saves STL files with proper filename to outputFolder
 # - saves properly named CSV lookup files for each MT
 # - current lists will create 144 files each for STL & CSV
 for genderID in ${genderIDs[@]}; do     # genders: 2
@@ -63,24 +56,33 @@ for genderID in ${genderIDs[@]}; do     # genders: 2
         for blocksizeID in ${blocksizeIDs[@]}; do   # blocksizes: 3
             blocksize=${blocksizeList[$blocksizeID]}
 
+            # create output subfolder if it doesn't exist------------
+            outputSubfolder=VH_${gender}_${organ}_${blocksize}_Millitomes
+            cd $outputFolder    # create it in outputFolder
+            if [ ! -d ${outputSubfolder} ]
+            then
+                mkdir ${outputSubfolder}
+            fi
+            cd - #change directory back to work directory
+
             for lateralityID in ${lateralityIDs[@]}; do # top/bottom: 2
                 laterality=${lateralityList[$lateralityID]}
 
                 for scaleID in ${scaleIDs[@]}; do       # scale: 3
                     scale=${scaleList[$scaleID]}
 
-                        touch $consoleOutput    # time stamp? creates the file?
-                        exec 3<&1               # save original stdout to 3
-                        exec &> $consoleOutput  # direct all out and err to the log file
-                      
-                        # runs openscad program, properly configured
-                        openscad ${mtGenerator} -o ${stlOutputFolder}/VH_${gender}_${organ}_${blocksize}_${scale}_${laterality}.stl -D laterality=${lateralityID} -D gender=${genderID} -D organ_id=${organID} -D organ_scale=${scaleID} -D block_size=${blocksize} -D output_flag=${outputFlag}
+                    touch $consoleOutput        # time stamp? creates the file?
+                    exec 3<&1               # save original stdout to 3
+                    exec &> $consoleOutput  # direct all out and err to the log file
+                    
+                    # runs openscad program, properly configured
+                    openscad ${mtGenerator} -o ${outputFolder}/${outputSubfolder}/VH_${gender}_${organ}_${blocksize}_${scale}_${laterality}.stl -D laterality=${lateralityID} -D gender=${genderID} -D organ_id=${organID} -D organ_scale=${scaleID} -D block_size=${blocksize} -D output_flag=${outputFlag}
+                           
+                    exec 1>&3 # restore original stdout
 
-                        exec 1>&3 # restore original stdout
+                    input=$consoleOutput    # use this file to parse column/row data from logfile; (outputFlag=2)
 
-                        input=$consoleOutput    # use this file to parse column/row data from logfile; (outputFlag=2)
-
-                        while read -r line      # read lines and parse for column & row data
+                    while read -r line      # read lines and parse for column & row data
                         do
                             if [[ $line =~ "ECHO: \">col:" ]] ; then
                                 line2=${line/ECHO: \">col:/}
@@ -93,37 +95,46 @@ for genderID in ${genderIDs[@]}; do     # genders: 2
                             fi
                         done < "$input"
 
-                        columnCount=$((columnsStr))         # make explicit int number of columns
-                        rowCount=$((rowsStr))               # explicit int number of rows
-                        
-                        # make name, filename & content of CSV
-                        csvName=VH_${gender}_${organ}_${blocksize}_${scale}_${laterality}
-                        csvFileName=${csvOutputFolder}${csvName}.csv
-                        csvLine1=${csvName},
-                        csvLine2="${genderName} ${organ2} ${blocksize}x${blocksize}mm,"
-                        csvLine3="Millitome ID,Sample ID,"
-                        
-                        # write lines to CSV file
-                        echo $csvLine1 > $csvFileName   # CSV name
-                        echo $csvLine2 >> $csvFileName  # CSV meta data
-                        echo $csvLine3 >> $csvFileName  # CSV column info
+                    columnCount=$((columnsStr))         # make explicit int number of columns
+                    rowCount=$((rowsStr))               # explicit int number of rows
+                    
+                    # make name, filename & content of CSV
+                    csvName=VH_${gender}_${organ}_${blocksize}_${scale}_${laterality}
+                    csvFileName=${outputSubfolder}/${csvName}.csv
+                    csvLine1=${csvName},
+                    csvLine2="${genderName} ${organ2} ${blocksize}x${blocksize}mm ${scale} ${laterality},"
+                    csvLine3="Millitome ID,Sample ID,"
+                    
+                    cd $outputFolder    # change directory to output folder
 
-                        # iterate column/row counters to create A1.....Nx matrix
-                        rowCounter=1                        # rows start at 1
-                        until [ $rowCounter -gt $rowsStr ]  # need to use 'strings' here
+                    # write lines to CSV file
+                    echo $csvLine1 > $csvFileName   # CSV name
+                    echo $csvLine2 >> $csvFileName  # CSV meta data
+                    echo $csvLine3 >> $csvFileName  # CSV column info
+
+                    # iterate column/row counters to create A1.....Nx matrix
+                    rowCounter=1                        # rows start at 1
+                    until [ $rowCounter -gt $rowsStr ]  # need to use 'strings' here
                         do
                             colCounter=0                    # columns start at 0 because of retrieval from asciiList
                             until [ $colCounter -eq $columnsStr ]
-                            do
-                                rowString=${asciiList[$colCounter]}$rowCounter, # assemble row string
-                                echo $rowString >> $csvFileName                 # row string to file
-                                ((colCounter++))
-                            done
+                                do
+                                    rowString=${asciiList[$colCounter]}$rowCounter, # assemble row string
+                                    echo $rowString >> $csvFileName                 # row string to file
+                                    ((colCounter++))
+                                done
 
-                             ((rowCounter++))
+                            ((rowCounter++))
                         done
+                        cd -    # back to main folder
                     done
                 done
+                # here after folder is done
+                # zip this folder
+                cd $outputFolder
+                zip -r ${outputSubfolder}.zip ${outputSubfolder}
+                rm -r ${outputSubfolder}
+                cd -
             done
         done
     done
